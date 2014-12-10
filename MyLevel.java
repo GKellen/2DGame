956bd39ef9d89cb2d5cdc6f92ef9ea4ed32c02d6
Gary Kellenberger <gary.kellenerber@gmail.com>
2014-12-10 17:20:23 -0500

    Update MyLevel.java
    added lines 13-83.
    
    note: would not compile. got 40 errors.

--- MyLevel.java
+++ MyLevel.java
@@ -9,3 +9,75 @@ class MyLevel extends Level {
 		super(levelWidth, levelHeight);
 	}
 }
+
+class MyLevelLayer extends LevelLayer {
+ MyLevelLayer(Lavel owner){
+  super(owner);
+  color blueishColor = color(0,130,255);
+  setBackgtoundColor(blueishColor);
+  MyThingy mt = new MyThingy();
+  mt.setPosition(width/2, height/2);
+  addPlayer(mt);
+  MyEnemy me = new MyEnemy();
+  addInteractor(me);
+  addBoundary(new Boundary(0,height, width,height));
+  addBoundary(new Boundary(width,height, width,0));
+  addBoundary(new Boundary(width,0,0,0));
+  addBoundary(new Boundary(0,0,0,height));
+ }
+}
+
+class MyLevel extends Level {
+ MyLeel(float levelWidth, float levelHeight){
+  super(levelWidth, levelWidth);
+  addScreenLayer("my lvel layer", new MyLevelLayer(this));
+ }
+}
+
+class MyThingy extends Player{
+ . . .
+}
+
+MyThingy() {
+ super("thingy");
+ setStates();
+ handleKey('W');
+ handleKey('A');
+ handleKey('S');
+ handleKey('D');
+ setImpulseCoefficients(0.75,0.75);
+}
+
+void setStates(){
+ addState(new State("idle", "idle.gif"));
+}
+
+void handleInput() {
+ if(isKeyDown('W')) { addImpulse(0,-1);}
+ if(isKeyDown('A')) { addImpulse(-1,0);}
+ if(isKeyDown('D')) { addImpulse(1,0);}
+ if(isKeyDown('S')) { addImpulse(0,1);}
+}
+
+class MyEnemy extends Interactor implements Traker{
+ MyEnemy() {
+  super("enemy");
+  setStates();
+ }
+ 
+ void setStates(){
+  addState(new State("idle", "idle_enemy.gif"));
+ }
+ 
+ void track(Actor actor, float x, float y, float w, float h) {
+  GenericTracker.track(this, actor, 0.05);
+ }
+}
+
+interface Tracker {
+ void track(Actor actor, float vx, float vy, float vw, flaot vh);
+}
+
+void track(Actor actor float vx, float vy, float vw, float vh) {
+ GenericTracker.track(this, actor, 0.05);
+}
